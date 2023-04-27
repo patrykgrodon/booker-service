4548987f46520f998652d1467d26de94a3f6e59c
Patryk Grodon <kyrtapek1@gmail.com>
2023-04-27 14:49:44 +0200

    Add PasswordField component

---
+++ src/common/components/PasswordField.tsx
@@ -0,0 +1,32 @@
+import { IconButton, TextField, TextFieldProps } from "@mui/material";
+import { VisibilityOutlined, VisibilityOffOutlined } from "@mui/icons-material";
+import { forwardRef, useState } from "react";
+
+const PasswordField = forwardRef(({ ...props }: TextFieldProps, ref: any) => {
+  const [isPasswordVisible, setPasswordVisible] = useState(false);
+  const togglePasswordVisible = () =>
+    setPasswordVisible((isVisible) => !isVisible);
+  return (
+    <TextField
+      {...props}
+      ref={ref}
+      type={isPasswordVisible ? "text" : "password"}
+      InputProps={{
+        endAdornment: (
+          <IconButton
+            size="small"
+            aria-label={isPasswordVisible ? "Hide password" : "Show password"}
+            onClick={togglePasswordVisible}
+          >
+            {isPasswordVisible ? (
+              <VisibilityOffOutlined />
+            ) : (
+              <VisibilityOutlined />
+            )}
+          </IconButton>
+        ),
+      }}
+    />
+  );
+});
+export default PasswordField;
