e49b861f4aea14c8d0b5aaaeba002da0689c1c8b
Patryk Grodon <kyrtapek1@gmail.com>
2023-04-27 14:50:47 +0200

    Add SubmitErrorMessage component

--- src/common/components/index.ts
+++ src/common/components/index.ts
@@ -1,4 +1,7 @@
 import Logo from "./Logo";
+import PasswordField from "./PasswordField";
+import RequestButton from "./RequestButton";
 import Spinner from "./Spinner";
+import SubmitErrorMessage from "./SubmitErrorMessage";
 
-export { Logo, Spinner };
+export { Logo, PasswordField, RequestButton, Spinner, SubmitErrorMessage };
