48bafbfa87e02889e28e72f2f27f6b8828c02539
Patryk Grodon <kyrtapek1@gmail.com>
2023-04-28 16:03:40 +0200

    Add Header component and use it in AppLayout

--- src/common/components/AppLayout/Header.tsx
+++ src/common/components/AppLayout/Header.tsx
@@ -1,9 +1,33 @@
+import { AppBar, Toolbar, IconButton, Box } from "@mui/material";
+import { Logo } from "..";
+import { Menu } from "@mui/icons-material";
+import UserMenu from "./UserMenu";
+
 type HeaderProps = {
   toggleSidebar: () => void;
 };
 
 const Header = ({ toggleSidebar }: HeaderProps) => {
-  return <div onClick={toggleSidebar}>Header</div>;
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton
+          size="large"
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          sx={{ mr: 2 }}
+          onClick={toggleSidebar}
+        >
+          <Menu />
+        </IconButton>
+        <Box sx={{ flexGrow: 1 }}>
+          <Logo />
+        </Box>
+        <UserMenu />
+      </Toolbar>
+    </AppBar>
+  );
 };
 
 export default Header;
