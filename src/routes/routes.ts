0b44e26ac3da407c41779aa6fcbf9082f5cb2b5a
Patryk Grodon <kyrtapek1@gmail.com>
2023-04-28 17:40:23 +0200

    Add services route

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -3,6 +3,7 @@ export const routes = {
   login: "/login",
   register: "/register",
   forgotPassword: "/forgot-password",
+  services: "/services",
 } as const;
 
 export type Route = (typeof routes)[keyof typeof routes];
