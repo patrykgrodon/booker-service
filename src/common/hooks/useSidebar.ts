58036fa480206f6b8efba2d69b88bf91e221cda0
Patryk Grodon <kyrtapek1@gmail.com>
2023-04-28 10:35:23 +0200

    Add useSidebar hook

---
+++ src/common/hooks/useSidebar.ts
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import { getLSItem, saveLSItem } from "common/utils/webStorage";
+
+const useSidebar = () => {
+  const sidebarLsName = "isSidebarOpen";
+  const sidebarLsValue = getLSItem<boolean>(sidebarLsName);
+  const [isOpen, setIsOpen] = useState(
+    sidebarLsValue === undefined ? true : sidebarLsValue
+  );
+
+  const toggleSidebar = () => {
+    setIsOpen((prevState) => {
+      saveLSItem(sidebarLsName, !prevState);
+      return !prevState;
+    });
+  };
+  return { isOpen, toggleSidebar };
+};
+
+export default useSidebar;
