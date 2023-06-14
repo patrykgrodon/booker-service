033e64bdcff03e4b3284a6289daf4475c15fa05e
Patryk Grodon <kyrtapek1@gmail.com>
2023-06-14 11:32:25 +0200

    Remove uppercase textTransform from default MuiButton props

--- src/theme.ts
+++ src/theme.ts
@@ -19,6 +19,7 @@ let theme = createTheme({
       defaultProps: {
         color: "primary",
         variant: "contained",
+        sx: { textTransform: "none" },
       },
     },
     MuiTextField: {
